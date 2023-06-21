42191b3c778047698196410fc743a8058a3d8922
Dikshant Kamboj <dikshant.kamboj.12@gmail.com>
2023-06-21 11:36:09 +0530

    first commit

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
 // FETCH DATA FROM SERVER
 
-    const response = await fetch('https://genie-iov1.onrender.com',{
+    const response = await fetch('http://localhost:5000',{
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -105,8 +105,6 @@ const handleSubmit = async (e) => {
         messageDiv.innerHTML="Something went wrong";
         alert(err); 
     }
-
-
 
 }
  
