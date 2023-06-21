df451132250ff59dfefd709759a88c8abc1676f3
Dikshant Kamboj <dikshant.kamboj.12@gmail.com>
2023-06-21 11:55:01 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
 // FETCH DATA FROM SERVER
 
-    const response = await fetch('http://localhost:5000',{
+    const response = await fetch('https://dcoder-z7ge.onrender.com',{
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
